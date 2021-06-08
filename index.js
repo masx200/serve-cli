bf01461b8123211f5bb39b99ea30957383e2efcf
masx200 <masx200@qq.com>
2021-06-08 11:29:14 +0800

    1 file modified

--- index.js
+++ index.js
@@ -1,3 +1,28 @@
-// Run `node index.js` in the terminal
+import cors from "@koa/cors";
+import http from "http";
+import Koa from "koa";
+import compress from "koa-compress";
+import conditional from "koa-conditional-get";
+import etag from "koa-etag";
 
-console.log(`Hello Node.js v${process.versions.node}!`);
+import logger from "koa-logger";
+import servestatic from "koa-static";
+import serveIndex from "koa2-serve-index";
+import path from "path";
+import process from "process";
+const app = new Koa();
+const publicpath = path.resolve(process.cwd());
+
+app.use(cors({}));
+app.use(logger({}));
+app.use(conditional());
+app.use(etag({}));
+app.use(compress({}));
+app.use(servestatic(publicpath, { hidden: true }));
+
+app.use(serveIndex(publicpath, { hidden: true }));
+const server = http.createServer(app.callback());
+
+server.listen(3000).on("listening", () => {
+    console.log(server.address());
+});
