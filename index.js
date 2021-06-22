a66da9fa1b6765c2b5a32ffe555a54554925709c
masx200 <masx200@qq.com>
2021-06-22 15:17:50 +0800

    loadcoremiddles

--- index.js
+++ index.js
@@ -1,35 +1,15 @@
-import cors from "@koa/cors";
-import range from "@masx200/koa-range";
-import streametag from "@masx200/koa-stream-etag";
 import Koa from "koa";
-import compress from "koa-compress";
-import conditional from "koa-conditional-get";
-import koaetag from "koa-etag";
-import servestatic from "koa-static";
-import serveIndex from "koa2-serve-index";
-import { AccessControlAllowOrigin } from "./AccessControlAllowOrigin.js";
-import { headhandler } from "./headhandler.js";
+import { loadcoremiddles } from "./loadcoremiddles.js";
 import { loggermiddles } from "./loggermiddles.js";
 import { publicpath } from "./publicpath.js";
 
 const app = new Koa();
-app.use(AccessControlAllowOrigin());
-// app.use(beforelogger());
-// app.use(afterlogger());
-app.use(headhandler());
-app.use(range);
-app.use(cors({}));
-// app.use(logger({}));
-app.use(conditional());
 
-app.use(compress({}));
-app.use(streametag({}));
-app.use(koaetag({}));
-app.use(servestatic(publicpath, { hidden: true }));
-
-app.use(serveIndex(publicpath, { hidden: true }));
-
-export { app };
 loggermiddles.forEach((middle) => {
     app.use(middle);
 });
+loadcoremiddles(app, publicpath);
+
+export { loadcoremiddles };
+
+export { app };
---
+++ loadcoremiddles.js
@@ -0,0 +1,28 @@
+import cors from "@koa/cors";
+import range from "@masx200/koa-range";
+import streametag from "@masx200/koa-stream-etag";
+import compress from "koa-compress";
+import conditional from "koa-conditional-get";
+import koaetag from "koa-etag";
+import servestatic from "koa-static";
+import serveIndex from "koa2-serve-index";
+import { AccessControlAllowOrigin } from "./AccessControlAllowOrigin.js";
+import { headhandler } from "./headhandler.js";
+
+export function loadcoremiddles(app, publicpath) {
+    app.use(AccessControlAllowOrigin());
+    // app.use(beforelogger());
+    // app.use(afterlogger());
+    app.use(headhandler());
+    app.use(range);
+    app.use(cors({}));
+    // app.use(logger({}));
+    app.use(conditional());
+
+    app.use(compress({}));
+    app.use(streametag({}));
+    app.use(koaetag({}));
+    app.use(servestatic(publicpath, { hidden: true }));
+
+    app.use(serveIndex(publicpath, { hidden: true }));
+}
