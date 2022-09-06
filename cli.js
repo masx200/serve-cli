daa14910a5a49a06d8cf65a1de31bb2ca8eb0e58
masx200 <34191203+masx200@users.noreply.github.com>
2022-09-06 19:09:10 +0800

    Update cli.js

--- cli.js
+++ cli.js
@@ -13,64 +13,73 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 const selfSignedKey = join(__dirname, "./certs/self-signed.key.pem");
 const selfSignedCert = join(__dirname, "./certs/self-signed.cert.pem");
-console.log("serve-cli");
-if (argv.help || argv.h) {
-    showhelp();
-}
-const config = {
-    path: argv.path || argv.pa || process.cwd(),
-    host: argv.host || argv.h || "0.0.0.0",
-    port: Number(argv.port || argv.po) || 4000,
 
-    ssl: Boolean(argv.ssl || argv.s),
-    sslKey: Boolean(argv.ssl || argv.s)
-        ? fs.readFileSync(argv.sslKey || selfSignedKey).toString()
-        : "",
-    sslCert: Boolean(argv.ssl || argv.s)
-        ? fs.readFileSync(argv.sslCert || selfSignedCert).toString()
-        : "",
-};
-console.log(
-    Object.fromEntries(
-        Object.entries(config).filter(([key]) => {
-            return !["sslKey", "sslCert"].includes(key);
-        })
-    )
-);
-const serverHandler = app.callback();
-const httpsopt = { cert: config.sslCert, key: config.sslKey, allowHTTP1: true };
-const server = config.ssl
-    ? http2.createSecureServer(httpsopt, serverHandler)
-    : http.createServer(serverHandler);
-server.on("error", (err) => {
-    if (err.code === "EADDRINUSE") {
-        console.error(err);
-        server.listen(Math.round(Math.random() * 65535));
-        return;
-    } else {
-        throw err;
+main(argv);
+export function main(argv) {
+    console.log("serve-cli");
+    if (argv.help || argv.h) {
+        showhelp();
     }
-});
+    const config = {
+        path: argv.path || argv.pa || process.cwd(),
+        host: argv.host || argv.h || "0.0.0.0",
+        port: Number(argv.port || argv.po) || 4000,
 
-server.on("listening", () => {
-    console.log(`Server listening on ` + JSON.stringify(server.address()));
-});
-function run() {
-    server.listen(config.port, config.host);
+        ssl: Boolean(argv.ssl || argv.s),
+        sslKey: Boolean(argv.ssl || argv.s)
+            ? fs.readFileSync(argv.sslKey || selfSignedKey).toString()
+            : "",
+        sslCert: Boolean(argv.ssl || argv.s)
+            ? fs.readFileSync(argv.sslCert || selfSignedCert).toString()
+            : "",
+    };
+    console.log(
+        Object.fromEntries(
+            Object.entries(config).filter(([key]) => {
+                return !["sslKey", "sslCert"].includes(key);
+            })
+        )
+    );
+    const serverHandler = app.callback();
+    const httpsopt = {
+        cert: config.sslCert,
+        key: config.sslKey,
+        allowHTTP1: true,
+    };
+    const server = config.ssl
+        ? http2.createSecureServer(httpsopt, serverHandler)
+        : http.createServer(serverHandler);
+    server.on("error", (err) => {
+        if (err.code === "EADDRINUSE") {
+            console.error(err);
+            server.listen(Math.round(Math.random() * 65535));
+            return;
+        } else {
+            throw err;
+        }
+    });
+
+    server.on("listening", () => {
+        console.log(`Server listening on ` + JSON.stringify(server.address()));
+    });
+    function run() {
+        server.listen(config.port, config.host);
+    }
+
+    process.on("unhandledRejection", (e) => {
+        console.error(e);
+        throw e;
+    });
+    const configurl = `${config.ssl ? "https" : "http"}://${config.host}:${
+        config.port
+    }`;
+    const logs = [
+        `Server running at ${configurl}`,
+
+        "Hit CTRL-C to stop the server",
+        "Run with --help to print help",
+    ];
+
+    console.log(logs.join("\n"));
+    run();
 }
-
-process.on("unhandledRejection", (e) => {
-    console.error(e);
-});
-const configurl = `${config.ssl ? "https" : "http"}://${config.host}:${
-    config.port
-}`;
-const logs = [
-    `Server running at ${configurl}`,
-
-    "Hit CTRL-C to stop the server",
-    "Run with --help to print help",
-];
-
-console.log(logs.join("\n"));
-run();
